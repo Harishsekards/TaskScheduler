a091b836699cc1bb383ae107a528f948abac5749
HARISH SEKAR <dsharish@outlook.com>
2020-06-23 12:08:56 +0530

    Add project files.

---
+++ TaskScheduler/MyScheduler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskScheduler
+{
+    public static class MyScheduler
+    {       
+        public static void IntervalInHours(int hour, int min, double interval, Action task)
+        {
+            SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        }
+    }
+}
